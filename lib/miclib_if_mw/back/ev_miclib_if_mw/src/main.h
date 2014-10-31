3737579578b3cdc4d8f06090bc57f5984c60a2d8
unknown <chadolmangi@cnbtec.com>
2014-10-31 13:17:16 +0900

    remove lib drectory

--- lib/miclib_if_mw/back/ev_miclib_if_mw/src/main.h
+++
@@ -1,8 +0,0 @@
-
-#include "miclib_type.h"
-#include "miclib_if_mw.h"
-
-void Win32PrintCall(uint08 x, uint08 y, uint32 ch);
-void Osd_Update();
-void Osd_Update_Flg(uint08 flg);
-void id_Print(uint16 id);
